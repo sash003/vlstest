775901ee1484045458db7415fa1090d3d8a4ea43
sash <ilikeitalls@mail.ru>
2015-10-17 16:31:39 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,84 +1,70 @@
 
 
-// Создает обработчик события window.onLoad
-        //YMaps.jQuery(function () {
-            // Создает экземпляр карты и привязывает его к созданному контейнеру
-            var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+    // Создает экземпляр карты и привязывает его к созданному контейнеру
+    var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+                
+    // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+    map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 11);
+                
+    map.addControl(new YMaps.TypeControl());
+    map.addControl(new YMaps.ToolBar());
+    map.addControl(new YMaps.Zoom());
+    map.addControl(new YMaps.ScaleLine());
+                         
             
-            // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
-            map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 11);
-            
-            map.addControl(new YMaps.TypeControl());
-            map.addControl(new YMaps.ToolBar());
-            map.addControl(new YMaps.Zoom());
-            map.addControl(new YMaps.ScaleLine());
-                     
-        //});
+    var d = document,
+    showMap = d.querySelector('.app_show'),
+    first = d.getElementById('first'),
+    second = d.getElementById('second'),
+    city = d.getElementById('city'),
+    show = d.getElementById('show'),
+    routeShow = d.getElementById('route');
         
-        var d = document,
-        showMap = d.querySelector('.app_show'),
-        first = d.getElementById('first'),
-        second = d.getElementById('second'),
-        city = d.getElementById('city'),
-        show = d.getElementById('show'),
-        routeShow = d.getElementById('route');
-    
     addEvent(showMap, 'click', function(){
         d.getElementById('YMapsID').show();
     });
-    
+        
     addEvent(show, 'click', function(e){
         preventdefault(e);
-        
+            
         var point1 = city.val() + ', ' + first.val(),
             point2 = city.val() + ', ' + second.val();
-            //console.log(point1, point2);
-            
-            
-        var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
-            
-            // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
-            map.setCenter(new YMaps.GeoPoint(37.64, 55.76), 10);
-            
-            map.addControl(new YMaps.TypeControl());
-            map.addControl(new YMaps.ToolBar());
-            map.addControl(new YMaps.Zoom());
-            map.addControl(new YMaps.ScaleLine());
-            
+                
         var router = new YMaps.Router(
-       // Список точек, которые необходимо посетить
-       [point1,point2],[],
-       { viewAutoApply: true }
-   );
-map.addOverlay(router); // Добавляет на карту полный маршрут
+           // Список точек, которые необходимо посетить
+           [point1,point2],[],
+           { viewAutoApply: true }
+       );
+        map.addOverlay(router); // Добавляет на карту полный маршрут
 
-YMaps.Events.observe(router, router.Events.Success, function () {
-   var route = router.getRoute(0);
-   var itineraryList = ['Трогаемся.'];
-   var action = [];
-   action['back'] = 'назад';
-   action['left'] = 'налево';
-   action['right'] = 'направо';
-   action['none'] = 'прямо';
-   
-   for (var i=0; i < route.getNumRouteSegments(); i++) {
-      var segment = route.getRouteSegment(i);
-      
-      if(action[segment.getAction()]){
-          way = action[segment.getAction()];
-      }
-      
-      if(segment.getStreet()){
-          Street = ' на ' + segment.getStreet();
-      }
-      itineraryList.push('Едем ' + way + Street + ', проезжаем ' + segment.getDistance() + ' м.');
-   }
-   
-   itineraryList.push('Останавливаемся.');
-   routeShow.html(itineraryList.join('<br>'));
-});
-   
-        
+        YMaps.Events.observe(router, router.Events.Success, function () {
+        var route = router.getRoute(0);
+        var itineraryList = ['Трогаемся.'];
+        var action = [];
+        action['back'] = 'назад';
+        action['left'] = 'налево';
+        action['right'] = 'направо';
+        action['none'] = 'прямо';
+       
+        for (var i=0; i < route.getNumRouteSegments(); i++) {
+          var segment = route.getRouteSegment(i);
+          
+          if(action[segment.getAction()]){
+              way = action[segment.getAction()];
+          }
+          
+          if(segment.getStreet()){
+              Street = ' на ' + segment.getStreet();
+          }
+          itineraryList.push('Едем ' + way + Street + ', проезжаем ' + segment.getDistance() + ' м.');
+        }
+       
+       itineraryList.push('Останавливаемся.');
+       routeShow.html(itineraryList.join('<br>'));
+       
+       });
+       
+            
     });
     
 /* кроссбраузерная функция отмены действия по умолчанию */
